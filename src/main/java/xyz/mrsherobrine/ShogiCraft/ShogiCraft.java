213306d1a133595aa88f31c52faafc178a71dfad
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-08-27 14:48:37 +0200

    initial commit

---
+++ src/main/java/xyz/mrsherobrine/ShogiCraft/ShogiCraft.java
@@ -0,0 +1,12 @@
+package xyz.mrsherobrine.ShogiCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShogiCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
