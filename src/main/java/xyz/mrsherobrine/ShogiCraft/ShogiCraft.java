7ffaa84c21558292fd60fa927da159ab47ad377f
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-08-27 15:27:55 +0200

    fix gitignore, get Board set up

--- src/main/java/xyz/mrsherobrine/ShogiCraft/ShogiCraft.java
+++ src/main/java/xyz/mrsherobrine/ShogiCraft/ShogiCraft.java
@@ -1,11 +1,15 @@
 package xyz.mrsherobrine.ShogiCraft;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.mrsherobrine.ShogiCraft.shogi.Board;
 
 public class ShogiCraft extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        new Board(0, 0, 20, 20, Bukkit.getPlayer("Mrs_Herobrine_").getUniqueId()).createNewBoard(this);
 
     }
 
--- src/main/java/xyz/mrsherobrine/ShogiCraft/shogi/Board.java
+++ src/main/java/xyz/mrsherobrine/ShogiCraft/shogi/Board.java
@@ -1,2 +1,66 @@
-package xyz.mrsherobrine.ShogiCraft.shogi;public class Board {
+package xyz.mrsherobrine.ShogiCraft.shogi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+public class Board {
+
+    private int lowerX;
+    private int lowerZ;
+    private int upperX;
+    private int upperZ;
+    private UUID owner;
+
+    public Board(int lowerX, int lowerZ, int upperX, int upperZ, UUID owner) {
+        this.lowerX = lowerX;
+        this.lowerZ = lowerZ;
+        this.upperX = upperX;
+        this.upperZ = upperZ;
+        this.owner = owner;
+    }
+
+    /**
+     * Get the board's bounds.
+     *
+     * @return the lower x and z, then the upper x and z coordinates in the world
+     */
+
+    public int[] getBounds() {
+        return new int[] {
+            this.lowerX, this.lowerZ, this.upperX, this.upperZ
+        };
+    }
+
+    /**
+     *
+     * @return lower x and z coordinates of the board
+     */
+
+    public int[] getLowerBound() {
+        return new int[] {
+             this.lowerX, this.lowerZ
+        };
+    }
+
+    /**
+     *
+     * @return upper x and z coordinates of the board
+     */
+
+    public int[] getUpperBound() {
+        return new int[] {
+            this.upperX, this.upperZ
+        };
+    }
+
+    public void createNewBoard(JavaPlugin plugin) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ()-> {
+            plugin.getLogger().info("Board with parameters: "+ Arrays.toString(getBounds()));
+            //TODO: insert stuff into db
+
+        });
+    }
 }
